80c0cc80a41a209ec6446b239e56cd4b3a756ff8
salmenz <salmen.zooro@gmail.com>
2020-02-03 20:21:19 +0000

    selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,15 +12,17 @@ void selection_sort(int *array, size_t size)
 size_t i, j, m;
 int aux;
 
-for (i = 0; i < size - 2; i++)
+for (i = 0; i < size - 1; i++)
 {
 m = i;
 for (j = i + 1; j < size; j++)
 if (array[j] < array[m])
 m = j;
+if (m != i)
+{
 aux = array[m];
 array[m] = array[i];
 array[i] = aux;
 print_array(array, size);
-}
+}}
 }
