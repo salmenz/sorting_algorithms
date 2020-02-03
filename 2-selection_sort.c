0a423a6fbeae491b00881ec8dfc870e74b6076a0
salmenz <salmen.zooro@gmail.com>
2020-02-03 17:35:03 +0000

    selection_sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * selection_sort - sorting array
+ * @array : array to sort
+ * @size : the size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+size_t i, j, m;
+int aux;
+
+for (i = 0; i < size - 1; i++)
+{
+m = i;
+for (j = i + 1; j < size; j++)
+if (array[j] < array[m])
+m = j;
+aux = array[m];
+array[m] = array[i];
+array[i] = aux;
+print_array(array, size);
+}
+}
