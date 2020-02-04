8bb9bf1500de5d6c8a08df98e9702f44e731a762
khalilholberton <1071@holbertonschool.com>
2020-02-04 00:38:16 +0000

    fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,5 +9,35 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *tempo;
+	if (list == NULL || *list == NULL)
+		return;
+	if((*list)->next == NULL)
+		return;
+	tempo = (*list->next);
+
+	while(tempo)
+	{
+		while(tempo->prev != NULL && tempo->prev->n > tempo->n)
+		{
+			tempo->prev->next = tempo->next;
+
+			if(tempo->next != NULL)
+				tempo->next->prev = tempo->prev;
+
+			tempo->next = tempo->prev;
+			tempo->prev = tempo->prev->prev;
+			tempo->next->prev = tempo;
+
+			if (tempo->prev = NULL)
+				*list = tempo;
+			else
+				tempo->prev->next = tempo;
+			print_list(*list);
+
+		}
+		tempo = tempo->next;
+
+	}
 
 }
