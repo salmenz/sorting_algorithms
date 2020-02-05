6780da1ddcc5e4729a8806e8ec8bc4507c70820a
khalilholberton <1071@holbertonschool.com>
2020-02-05 20:16:50 +0000

    fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,10 @@
 #include "sort.h"
-
+/**
+ * swap_val - function that swap values of array
+ * @val1 : first value
+ * @val2 : second value
+ * Return : nothing
+ */
 void swap_val(int *val1, int *val2)
 {
 
@@ -8,17 +13,29 @@ void swap_val(int *val1, int *val2)
 	*val1 = *val2;
 	*val2 = tempo;
 }
+/**
+ * quick_sort - run the func quick
+ * @array : array to be sorted
+ * @size : size of the array
+ */
 void quick_sort(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
 		return;
 	quick(array, 0, size - 1, size);
 }
+/**
+ * quick - recursion func
+ * @array : array needed for sorting
+ * @low : first index
+ * @high : last index
+ * @size : size
+ */
 void quick(int *array, size_t  low, size_t high, size_t size)
 {
 	size_t pivot_idx;
 
-	if (high == 0 )
+	if (high == 0)
 		return;
 
 	if (high <= low)
@@ -30,6 +47,13 @@ void quick(int *array, size_t  low, size_t high, size_t size)
 	if (pivot_idx < size - 1)
 		quick(array, pivot_idx + 1, high, size);
 }
+/**
+ * partition - make the initial sort
+ * @array : array for sorting
+ * @low : first index
+ * @high : last index
+ * @size : size
+ */
 size_t partition(int *array, size_t low, size_t high, size_t size)
 {
 	size_t  i;
